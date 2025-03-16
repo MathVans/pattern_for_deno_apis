71f7d46fcea5bebf8677775dfccec7a6fe3e5f53
Mateus Almeida <mateusalmeida0070@gmail.com>
2025-03-15 23:39:03 -0300

    feat: adding the modules to the pattern

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { Hono } from "npm:hono";
-import { logger } from "./src/middlewares/logger.ts";
+// import { logger } from "./src/middlewares/logger.ts";
 
 const app = new Hono();
 
 // Global middleware
-app.use("*", logger);
+// app.use("*", logger);
 
 Deno.serve(app.fetch);
